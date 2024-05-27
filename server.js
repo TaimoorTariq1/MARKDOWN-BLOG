690c95706dc773ef18cbfa428b5eeb30d4d12fd1
Taimoor Codes <132977742+TaimoorTariq1@users.noreply.github.com>
2024-05-27 11:43:49 +0500

    .

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ app.use("/articles", articleRouter);
 app.get("/", (req, res) => {
   const articles = [
     {
-      title: "Test Article",
-      createAt: Date.now(),
+      title: 'Test Article',
+      createAt: new Date(),
       descripiton: "Test descripition",
     },
     {
-        title: "Test Article 2",
-        createAt: Date.now(),
+        title: 'Test Article 2',
+        createAt: new Date(),
         descripiton: "Test descripition 2",
       }
   ];
 
-  res.render("index", { articles: articles });
+  res.render('articles/index', { articles: articles });
 });
 
 app.listen(5000);
