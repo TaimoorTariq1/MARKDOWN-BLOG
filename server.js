a9ba524d195772f71c0f8b989737f7ade04aad6a
Taimoor Codes <132977742+TaimoorTariq1@users.noreply.github.com>
2024-06-06 23:44:33 +0500

    .

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
-const express = require("express");
+const express = require('express')
 const mongoose = require('mongoose')
-const articleRouter = require("./routes/articles");
-const app = express();
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
 mongoose.connect('mongodb://localhost/blog', {
-  useNewURLParser: true, useUnifiedTopology: true
-
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
-app.set("view engine", "ejs");
-
-app.use("/articles", articleRouter);
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: 'Test Article',
-      createAt: new Date(),
-      descripiton: "Test descripition",
-    },
-    {
-        title: 'Test Article 2',
-        createAt: new Date(),
-        descripiton: "Test descripition 2",
-      }
-  ];
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
-  res.render('articles/index', { articles: articles });
-});
+app.use('/articles', articleRouter)
 
-app.listen(5000);
+app.listen(5000)
